37e0e8787ff1b8cbbc8b9c353f771dd6c129c875
pradeepmshukla <53944087+pradeepmshukla@users.noreply.github.com>
2023-09-01 10:49:18 +0530

    Add files via upload
    react4assignment done

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,17 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Cost</h2>
+      <p>
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
+      </p>
+      <p>
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
